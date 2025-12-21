f8efc7f5526667b1f541c96bd55f6cda0f6bae47
Julio Faerman <julio@faermanj.com>
2025-12-21 05:45:43 +0100

    Add Flutter app and devcontainer setup
    - Created presence_app Flutter project
    - Added .devcontainer configuration for Flutter development
    - Set up Android build environment
    - Successfully built Android APK

---
+++ presence_app/lib/main.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+import 'package:presence_app/login_screen.dart';
+import 'package:presence_app/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Presence App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginScreen(),
+        '/home': (context) => const HomeScreen(),
+      },
+    );
+  }
+}
