82ec13bdd7cbdd75ac0630475d4cd2aad3460a81
Julio Faerman <julio@faermanj.com>
2026-02-03 23:38:38 +0100

    devbox update

--- scripts/devbox-init-hook.sh
+++ scripts/devbox-init-hook.sh
@@ -5,6 +5,7 @@ pushd "$DIR/.."
 echo "script [$0] started"
 #
 
+# Node setup
 if ! command -v node &> /dev/null; then
   NVM_URL="https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh"
   # Setup nodejs using nvm
@@ -14,6 +15,35 @@ if ! command -v node &> /dev/null; then
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
   nvm install --lts
   ln -s "$(which node)" "$HOME/.local/bin/node"
+fi
+
+# Docker
+if ! command -v docker &> /dev/null; then
+
+echo "Docker not found, installing..."
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
 fi
 
 
