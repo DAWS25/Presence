8ce7739a178c84e8d8195d2886f32385e5b06c64
Julio Faerman <julio@faermanj.com>
2026-02-12 21:27:42 +0100

    Remove node_modules copy to target and npm install from build

--- scripts/env-build.sh
+++ scripts/env-build.sh
@@ -5,12 +5,6 @@ DIR="$(dirname "$SCRIPT_DIR")"
 # 
 echo "📦 Packaging Presence for deployment..."
 WEB_DIR="$DIR/presence_web"
-# Check if node_modules is installed
-if [ ! -d "$WEB_DIR/node_modules" ]; then
-    echo "📦 Installing npm dependencies..."
-    cd "$WEB_DIR"
-    npm install
-fi
 
 # Clean and prepare target folder
 echo "📂 Preparing target folder..."
@@ -19,9 +13,6 @@ mkdir -p $WEB_DIR/target
 
 # Copy src files to target
 cp -a $WEB_DIR/src/* $WEB_DIR/target/
-
-# Copy node_modules to target (required for Bootstrap and face-api.js)
-cp -a $WEB_DIR/node_modules $WEB_DIR/target/
 
 echo "🔧 Building SAM API function..."
 pushd $DIR/presence_sam
